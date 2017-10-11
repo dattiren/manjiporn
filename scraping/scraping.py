d9971b911be68572ae8dcd18a3238fa4edd654cb
Kenta Iwami <g2117009@fun.ac.jp>
2017-10-12 01:22:00 +0900

    create db connection, sample html and .gitignore file

---
+++ scraping/scraping.py
@@ -0,0 +1,45 @@
+# ますたべ
+# すべてから無修正タグ、タイトルを除く
+# 詳細画面へ遷移し、そのページのリザーブurlを取得して保存
+
+# pip install mysqlclient
+import MySQLdb
+import setting as Setting
+
+
+def db_conect():
+    connect = MySQLdb.connect(
+        host=Setting.HOST,
+        user=Setting.USERNAME,
+        passwd=Setting.PASSWORD,
+        db=Setting.DB_NAME,
+        charset=Setting.CHARSET
+    )
+
+    cursor = connect.cursor()
+
+    return connect, cursor
+
+
+def db_close(connect, cursor):
+    cursor.close()
+    connect.close()
+
+
+def hoge(connect, cursor):
+    cursor.execute('select * from admin_users;')
+
+    for row in cursor:
+        print(row[0],row[1])
+
+
+
+
+def main():
+    connect, cursor = db_conect()
+    hoge(connect, cursor)
+    db_close(connect, cursor)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scraping/setting.py
@@ -0,0 +1,6 @@
+# DB Setting
+HOST = 'localhost'
+USERNAME = 'root'
+PASSWORD = 'root'
+DB_NAME = 'laravel_test'
+CHARSET = 'utf8'
