ba693ab5aa0122a7c4853d3060cb42e6b701d07e
小倉優悟 <yugo@ogurayuusatorunoMacBook-Pro.local>
2017-11-15 16:52:46 +0900

    [add]メインコンテンツのループを分割、動画にもリンク付け

--- front_end/playback.php
+++ front_end/playback.php
@@ -41,28 +41,7 @@
 			</section>
 			<section class="container-sections-one">
 			<h1>Others Recommended</h1>
-			<?php for($i=0;$i<=10;$i++): ?>
-				<article class="container-sections-one-movie">
-					<img src="rocket.jpg" alt="">
-					<div class="container-sections-one-movie-info">
-						<a class="container-sections-one-movie-info-link" href="playback.php">
-							<h2>Big tits sample big tits sampleadf aef;w eihfd'q oefhia eorif hq[oriag hfae[ori big tits sample big tits sampleadfaef;weihf　d'qoefhiaeo　rifhq[oria　ghfae[or　っっっっ　ｄi</h2>
-						</a>
-						<div class="container-sections-one-movie-info-infomation">
-							<span>
-								<i class="fa fa-play-circle" aria-hidden="true"></i>44444</span>
-							<span>
-								<i class="fa fa-folder" aria-hidden="true"></i>ddddd</span>
-						</div>
-						<div class="container-sections-one-movie-info-category">
-							<a>wawawa</a>
-							<a>fefefefefe</a>
-							<a>tintintintin</a>
-							<a>ponponpnop</a>
-						</div>
-					</div>
-				</article>
-			<?php endfor; ?>
+			<?php include('article_loop.php'); ?>
 		</section>
 		</div>
 		<?php include('side_nav.php'); ?>
